5cb2b34debc18a398bce196da2aa9cd9897eb8fa
Klaus Martin Vare <martinv4@users.noreply.github.com>
2019-09-09 11:20:47 +0300

    õppetöö jaoks

---
+++ 02tund/try.php
@@ -0,0 +1,33 @@
+<?php
+  $userName="Martin Vare";
+  $fullTimeNow=date("d/m/Y H:i:s");
+  $hourNow=date("H");
+  $partOfDay="hägune aeg";
+  if($hourNow < 8) {
+	$partOfDay="varane hommik";
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title> 
+  <?php echo $userName; ?>
+  Veebiproge
+  </title>
+</head>
+<body>
+  <?php echo "<h1>" . $userName . " koolitöö leht</h1>"; ?>
+  
+  <p>See leht on loodud koolitöö raames ja ei sisalda tõsiseltvõetavat sisu</p>
+  <p> siin on uus ja v2rkse tekst</p>
+  <hr>
+  <p>Lehe avamise hetkel oli aeg:
+  <?php echo $fullTimeNow; ?>. </p>
+  <?php echo "Lehe avamise hetkel oli " .$partOfDay ?>.
+</body>
+</html>
+
+https://martinv4@github.com/martinv4/veebiprogrammeerimine.git
+martinv4@users.noreply.github.com
